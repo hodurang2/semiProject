3c219531e73058afb8571cc853d2496c7a5a6670
hodurang2 <cchc0427@gmail.com>
2023-11-09 20:11:10 +0900

    20231109

---
+++ src/main/java/com/gdu/second/controller/MvcController.java
@@ -0,0 +1,14 @@
+package com.gdu.second.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MvcController {
+
+  @GetMapping({"/", "/main.do"})
+  public String main() {
+    return "layout/main";
+  }
+  
+}
