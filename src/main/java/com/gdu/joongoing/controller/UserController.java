c2cfbd224f1c0e14227f3e9e37e017f1f1cc6c01
wnals9 <wnals16@naver.com>
2023-11-14 20:19:19 +0900

    주민커밋

--- src/main/java/com/gdu/joongoing/controller/UserController.java
+++ src/main/java/com/gdu/joongoing/controller/UserController.java
@@ -31,6 +31,8 @@ public class UserController {
     // referer : 이전 주소가 저장되는 요청 Header 값
     String referer = request.getHeader("referer");
     model.addAttribute("referer", referer == null ? request.getContextPath() + "/main.do" : referer);
+    // 네이버 로그인
+    model.addAttribute("naverLoginURL", userService.getNaverLoginURL(request));
     return "user/login";
   }
   
@@ -102,7 +104,35 @@ public class UserController {
     user.setEmail(request.getParameter("email"));
     user.setName(request.getParameter("name"));
     userService.findPw(user, response);
-    
+  }
+  
+  @GetMapping("/naver/getAccessToken.do")
+  public String getAccessToken(HttpServletRequest request) throws Exception {
+    // 네이버로그인-2
+    String accessToken = userService.getNaverLoginAccessToken(request);
+    return "redirect:/user/naver/getProfile.do?accessToken=" + accessToken;
+  }
+  
+  @GetMapping("/naver/getProfile.do")
+  public String getProfile(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+    // 네이버로그인-3
+    UserDto naverProfile = userService.getNaverProfile(request.getParameter("accessToken"));
+    // 네이버로그인 후속 작업(처음 시도 : 간편가입, 이미 가입 : 로그인)
+    UserDto user = userService.getUser(naverProfile.getEmail());
+    if(user == null) {
+      // 네이버 간편가입 페이지로 이동
+      model.addAttribute("naverProfile", naverProfile);
+      return "user/naver_join";
+    } else {
+      // naverProfile로 로그인 처리하기
+      userService.naverLogin(request, response, naverProfile);
+      return "redirect:/main.do";
+    }
+  }
+  
+  @PostMapping("/naver/join.do")
+  public void naverJoin(HttpServletRequest request, HttpServletResponse response) {
+    userService.naverJoin(request, response);
   }
   
   
