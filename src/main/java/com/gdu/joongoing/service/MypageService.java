595c71f5ccc5dd604af4ae2b166f42a4fd69587d
kdh23232 <kdh23232@naver.com>
2023-11-15 14:14:44 +0900

    다혜커밋1115

--- src/main/java/com/gdu/joongoing/service/MypageService.java
+++ src/main/java/com/gdu/joongoing/service/MypageService.java
@@ -10,9 +10,13 @@ import org.springframework.http.ResponseEntity;
 import com.gdu.joongoing.dto.UserDto;
 
 public interface MypageService {
+  // 마이페이지
   public UserDto getUser2(String email);
   public void modifyPw(HttpServletRequest request, HttpServletResponse response);
   public void modifyInterest(HttpServletRequest request, HttpServletResponse response);
   public ResponseEntity<Map<String, Object>> modify(HttpServletRequest request);
+  
+  // 판매목록
+  public Map<String, Object> getSalesList(HttpServletRequest request);
   
 }
